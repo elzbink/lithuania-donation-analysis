f804a28495c21422b96bc6eb770317e6b42854d4
elzbink <ebinkauskaite@gmail.com>
2026-06-06 15:35:20 +0300

    Home.py

---
+++ Home.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title("Donation Analysis")
+
+st.markdown("""
+**Group members:**
+- Morta Aleksiejūnaitė  
+- Danielius Andrejevas  
+- Elzė Binkauskaitė  
+- Idris Kloufi  
+- Liepa Žvirblytė
+""")
