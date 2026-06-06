bd1b9cd6eee2d879f33aa57ca8bc9560a8820ed1
Elzė Binkauskaitė <ebinkauskaite@gmail.com>
2026-06-06 16:15:59 +0300

    Update data_prep.py

--- data_prep.py
+++ data_prep.py
@@ -4,8 +4,7 @@ import streamlit as st
 @st.cache_data
 def load_data():
     url = "https://www.dropbox.com/scl/fi/s0e5f5mj6mxgqh6jkjqdg/Deklaracija.parquet?rlkey=rdkx6gnzbddag6psam7g34l2n&st=oxs84wsk&raw=1"
-    df = pd.read_parquet(url)
+    df = pd.read_parquet(url, columns=['metai', 'suma', 'gavejo_savivaldybe', 'gavejo_tipas', 'gavejo_imones_rusis'])
     df = df[df['suma'] > 0].copy()
-    df.drop(columns=['_type', '_id', '_revision', '_page.next', 'eiles_nr'], inplace=True)
     df_clean = df.dropna(subset=['suma'])
     return df_clean
